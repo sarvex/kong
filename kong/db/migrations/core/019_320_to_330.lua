952283dc5687d7bed26bd88b2f3432d77ea8f8d5
Michael Martin <flrgh@protonmail.com>
2023-04-10 12:42:15 -0700

    chore(wasmx): revise data and execution models (#10589)
    * chore(wasm): remove .protocols field
    
    * chore(wasm): change scope behavior
    
    * Remove route+service filter chain attachment
    * Ensure only one filter chain per route/service/global scope
    
    * chore(wasm): refactor and add cache tests
    
    * chore(wasmx): make one of service/route required
    
    * feat(wasm): rebuild and cache filter chain state
    
    * revert ngx wasm module branch change
    
    * fix constraint
    
    * add dbless tests
    
    * add doc strings and clean up some code
    
    * add tests for chains with no enabled filters
    
    * handle strict worker consistency
    
    * address some readability PR comments
    
    * chore(wasm): add guards for unreachable branches
    
    * chore(wasm): add label for log entries
    
    This computes and adds a label to each filter chain reference so that we
    can use it in log entries. The label consists of the service ID and
    route ID that are associated with the filter chain reference.
    
    * chore(wasm): improve readability
    
    * fix(wasm): add missing proxy_wasm.start() call

--- kong/db/migrations/core/019_320_to_330.lua
+++ kong/db/migrations/core/019_320_to_330.lua
@@ -8,7 +8,6 @@ return {
             -- Do nothing, accept existing state
           END;
       $$;
-
       DO $$
           BEGIN
           ALTER TABLE IF EXISTS ONLY "ca_certificates" ADD "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'UTC');
@@ -96,7 +95,6 @@ return {
 
 
       DROP TRIGGER IF EXISTS "clustering_data_planes_ttl_trigger" ON "clustering_data_planes";
-
       DO $$
       BEGIN
         CREATE TRIGGER "clustering_data_planes_ttl_trigger"
@@ -107,24 +105,31 @@ return {
         -- Do nothing, accept existing state
       END$$;
 
-
       CREATE TABLE IF NOT EXISTS "wasm_filter_chains" (
         "id"          UUID                       PRIMARY KEY,
-        "name"        TEXT                       UNIQUE,
         "enabled"     BOOLEAN                    DEFAULT TRUE,
         "route_id"    UUID                       REFERENCES "routes"     ("id") ON DELETE CASCADE,
         "service_id"  UUID                       REFERENCES "services"   ("id") ON DELETE CASCADE,
         "ws_id"       UUID                       REFERENCES "workspaces" ("id") ON DELETE CASCADE,
-        "protocols"   TEXT[],
+        "cache_key"   TEXT                       UNIQUE,
         "filters"     JSONB[],
         "tags"        TEXT[],
         "created_at"  TIMESTAMP WITH TIME ZONE,
-        "updated_at"  TIMESTAMP WITH TIME ZONE
+        "updated_at"  TIMESTAMP WITH TIME ZONE,
+
+        -- service and route are mutually exclusive
+        CONSTRAINT "wasm_filter_chains_scope_ck"
+          CHECK ((route_id IS NULL     AND service_id IS NOT NULL)
+              OR (route_id IS NOT NULL AND service_id IS NULL))
       );
+
+      CREATE UNIQUE INDEX IF NOT EXISTS "wasm_filter_chains_cache_key_idx"
+        ON "wasm_filter_chains" ("cache_key");
+      END$$;
 
       DO $$
       BEGIN
-        CREATE INDEX IF NOT EXISTS "wasm_filter_chains_name_idx" ON "wasm_filter_chains" ("name");
+        CREATE INDEX IF NOT EXISTS "wasm_filter_chains_tags_idx" ON "wasm_filter_chains" USING GIN ("tags");
       EXCEPTION WHEN UNDEFINED_COLUMN then
         -- do nothing, accept existing state
       END$$;
